8522bb3b0290dca331aba76bbe37b6d2a7e8d6c8
cronokirby <cronokirby@gmail.com>
2017-04-08 21:42:44 +0200

    initial commit

---
+++ aldash/lib/aldash.ex
@@ -0,0 +1,18 @@
+defmodule Aldash do
+  @moduledoc """
+  Documentation for Aldash.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aldash.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aldash/test/aldash_test.exs
@@ -0,0 +1,8 @@
+defmodule AldashTest do
+  use ExUnit.Case
+  doctest Aldash
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

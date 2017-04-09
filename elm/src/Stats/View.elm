b88aa341eb612b439939a9ed778b8f065a5b3842
cronokirby <cronokirby@gmail.com>
2017-04-09 22:28:49 +0200

    adds basic table formatting

--- elm/src/Stats/View.elm
+++ elm/src/Stats/View.elm
@@ -2,13 +2,36 @@ module Stats.View exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
+import List exposing (..)
 
-import Stats.Models exposing (Model)
+import Stats.Models exposing (Model, Uptime)
 import Stats.Update exposing (Msg(..))
+
+
+formatUptime : Uptime -> String
+formatUptime {days, hours, minutes} =
+  foldr (++) "" <|
+    map2 (\a b -> if a == 0 then ""
+                  else toString a ++ b)
+         [days, hours, minutes]
+         ["d ", "h ", "m"]
+
+cell : (List (Attribute msg) -> List (Html msg) -> Html msg) ->
+       String -> Html msg
+cell f x = f [] [Html.text x]
 
 view : Model -> Html Msg
 view model =
   div []
-    [ text (toString model)
+    [ table []
+        [ tr []
+           [ cell th "Processes"
+           , cell th "Uptime"
+           ]
+        , tr []
+           [ cell td <| toString model.processes
+           , cell td <| formatUptime model.uptime
+           ]
+        ]
     , button [ onClick Refresh ] [ text "refresh" ]
     ]

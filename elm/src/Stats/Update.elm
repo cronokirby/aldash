2d8133aa9357b3cb5d5532adb762ed0ff75677cb
cronokirby <cronokirby@gmail.com>
2017-04-09 17:45:42 +0200

    simplifies the types in stats

--- elm/src/Stats/Update.elm
+++ elm/src/Stats/Update.elm
@@ -5,22 +5,20 @@ import Json.Decode as Decode exposing (..)
 import Stats.Models exposing (Model)
 
 
-type alias Statinfo = { processes : Int }
-
-getStats : Http.Request Statinfo
+getStats : Http.Request Model
 getStats = Http.get "http://localhost:8080/stats" decodeStats
 
-decodeStats : Decoder Statinfo
-decodeStats = Decode.map Statinfo (field "processes" int)
+decodeStats : Decoder Model
+decodeStats = Decode.map Model (field "processes" int)
 
-type Msg = Change (Result Http.Error Statinfo)
+type Msg = Change (Result Http.Error Model)
          | Refresh
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
   Refresh ->
     (model, Http.send Change getStats)
-  Change (Ok new) -> -- Swap out the old number
-    ({ model | processes = new.processes }, Cmd.none)
+  Change (Ok new) -> -- Swap out the entire thing
+    (new, Cmd.none)
   Change (Err why) ->
     (model, Cmd.none)

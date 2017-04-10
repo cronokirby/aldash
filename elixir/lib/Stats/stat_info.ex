92f5e5fa8003e8882b7a82f899f47a70a53bd0b1
cronokirby <cronokirby@gmail.com>
2017-04-10 14:15:34 +0200

    adds fuller info to stats

--- elixir/lib/Stats/stat_info.ex
+++ elixir/lib/Stats/stat_info.ex
@@ -7,9 +7,22 @@ defmodule Aldash.Stats.StatInfo do
     {days, hours} = {div(hours, 60), rem(hours, 60)}
     %{"d" => days, "h" => hours, "m" => min}
   end
+
   def full_stats do
     processes = length :erlang.processes()
+    memory = :erlang.memory()
+    memories =
+      [:total, :code, :process, :atom, :ets]
+      |> Stream.zip([1000, 1000, 1000, 1_000_000, 1_000_000])
+      Enum.map(fn {atom, size} ->
+        memory[atom] / size
+      end)
+    {{_, io_input}, {_, io_output}}  = :erlang.statistics(:io)
+    info = [processes, uptime(), io_input / 1000, io_output / 100] ++
+           memories
+    keys = ["processes", "uptime", "IOin", "IOout",
+            "totalmem", "procmem", "codemem", "atmem", "ETSmem"]
     # We use string keys since we'll be converting this to json shortly
-    %{"processes" => processes, "uptime" => uptime()}
+    Stream.zip(keys, info) |> Enum.into(%{})
   end
 end
--- elixir/test/Stats/stat_info_test.exs
+++ elixir/test/Stats/stat_info_test.exs
@@ -7,9 +7,18 @@ defmodule Aldash.Stats.StatInfoTest do
     uptime = StatInfo.uptime()
     assert %{"d" => 0, "h" => 0, "m" => _} = uptime
   end
+
   test "Status about the current system should be returned" do
     info = StatInfo.full_stats()
     assert is_integer(info["processes"])
     assert is_integer(info["uptime"]["m"])
+  end
+
+  test "Full information should be given" do
+    info = StatInfo.full_stats()
+    Enum.each ["uptime", "processes", "totalmem",
+               "codemem", "IOin", "IOout",
+               "procmem", "ETSmem", "atmem"],
+               fn k -> assert Map.has_key?(info, k) end
   end
 end
